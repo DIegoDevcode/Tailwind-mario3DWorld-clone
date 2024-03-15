6a13d467324f6ad1ba244dd154d778ade53c5a8d
DIegoDevcode <jdiegomoreno05@gmail.com>
2024-03-15 12:04:57 -0500

    Creando breakpoints personalizados y minificando el css.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
       backgroundImage: theme => ({
         'wave-pattern': 'url(../img/wave-white.png)',
         'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
-      })
+      }),
+      screens:{
+        'xsm': '320px'
+      }
     },
   },
   plugins: [],
