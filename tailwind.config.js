270ce1b07bf58d67364006b75ebb7d05790eb5e6
DIegoDevcode <jdiegomoreno05@gmail.com>
2024-03-18 20:00:00 -0500

    Done cambio de rutas alternativas

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
-        'wave-pattern': 'url(../img/wave-white.png)',
-        'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
-        'pattern-characters-red': "url('../img/pattern-characters-red.png')",
-        'character-mario': "url('../img/character-l.png')",
-        'wave-pink': "url('../img/wave-pink.png')",
-        'yellow-dots': "url('../img/pattern-yellow-dots.png')",
+        'wave-pattern': 'url(../src/img/wave-white.png)',
+        'wave-pattern-dots': 'url(../src/img/pattern-white-dots.png)',
+        'pattern-characters-red': "url('../src/img/pattern-characters-red.png')",
+        'character-mario': "url('../src/img/character-l.png')",
+        'wave-pink': "url('../src/img/wave-pink.png')",
+        'yellow-dots': "url('../src/img/pattern-yellow-dots.png')",
       }),
       screens:{
         'xsm': '320px',
